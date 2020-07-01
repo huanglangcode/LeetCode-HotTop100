7929017f04e27ffebd2305b9018ea9fd7d034ebe
lang.huang <lang.huang@li.local>
2020-07-01 19:54:05 +0800

    33

--- 33.search-in-rotated-sorted-array.js
+++ 33.search-in-rotated-sorted-array.js
@@ -17,16 +17,30 @@ var search = function (nums, target) {
   let left = 0
   let right = nums.length - 1
   while (left <= right) {
-    let mid = left + (right - left >> 2)
-    console.log('mid :>> ', mid)
-    //确定target是否在升序的部分里 如果在的话进行二分查找. 如果不在 继续二分
-    //left mid,  mid+1 right 
-    if (nums[mid] === target) { return mid }
-    //todo
+    let mid = left + (right - left >> 1)
+    console.log('mid :>> ', mid, 'left: ', left, 'right: ', right)
+    console.log('compare: ', target, nums[mid], nums[0])
+    if (nums[mid] === target) {
+      console.log('binggo :>> ', mid)
+      return mid
+    }
+    if (nums[0] <= nums[mid]) {
+      if (nums[0] <= target && target < nums[mid]) {
+        right = mid - 1
+      } else {
+        left = mid + 1
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[nums.length - 1]) {
+        left = mid + 1
+      } else {
+        right = mid - 1
+      }
+    }
   }
-  return -1
+  return  -1
 };
 
-search([5, 6, 7, 8, 2, 3, 4], 4)
+search([4, 5, 6, 7, 0, 1, 2], 5)
 // @lc code=end
 
