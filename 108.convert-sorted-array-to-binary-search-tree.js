a00113cdf920a20ed6738ddeb4e8098c5ebc2bf6
lang.huang <lang.huang@li.local>
2020-07-03 13:52:01 +0800

    108

---
+++ 108.convert-sorted-array-to-binary-search-tree.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=108 lang=javascript
+ *
+ * [108] Convert Sorted Array to Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+  if (!nums.length) return null
+  
+  const mid = Math.floor(nums.length / 2)
+  const root = new TreeNode(nums[mid])
+
+  root.left = sortedArrayToBST(nums.slice(0, mid))
+  root.right = sortedArrayToBST(nums.slice(mid + 1))
+  
+  return root
+};
+
+// class TreeNode {
+//   constructor(val, left, right) {
+//     this.val = (val === undefined ? 0 : val);
+//     this.left = (left === undefined ? null : left);
+//     this.right = (right === undefined ? null : right);
+//   }
+// }
+
+sortedArrayToBST([-10, -3, 0, 5, 9])
+// @lc code=end
+
