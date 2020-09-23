f4ab7738e7238a8dc30e00d9b3c4a1b45d85f967
lang.huang <lang.huang@cyclone-robotics.com>
2020-09-23 13:41:50 +0800

    1

--- 34.find-first-and-last-position-of-element-in-sorted-array.js
+++ 34.find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,53 +3,15 @@
  *
  * [34] Find First and Last Position of Element in Sorted Array
  */
-// @lc code=start
-// /**
-//  * @param {number[]} nums
-//  * @param {number} target
-//  * @return {number[]}
-//  */
-// var searchRange = function(nums, target) {
-// };
-class CQueue {
-  constructor() {
-    this.A = []
-    this.B = []
-  }
-  /**
-   * @param {number} value
-   * @return {void}
-   */
-  appendTail(value) {
-    this.A.push(value)
-  }
-  /**
-   * @return {number}
-   */
-  deleteHead() {
-    if (this.B.length !== 0) {
-      return this.B.pop()
-    }
-    if (this.A.length === 0) {
-      return -1
-    }
-    while (this.A.length !== 0) {
-      this.B.push(this.A.pop())
-    }
-    return this.B.pop()
-  }
-}
-let cq = new CQueue()
-console.log('cq :>> ', cq, cq.A, cq.B)
-cq.appendTail(1)
-var b = cq.deleteHead()
-console.log('b :>> ', b)
 
+// @lc code=start
 /**
- * Your CQueue object will be instantiated and called as such:
- * var obj = new CQueue()
- * obj.appendTail(value)
- * var param_2 = obj.deleteHead()
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
  */
+var searchRange = function(nums, target) {
+    //1
+};
 // @lc code=end
 
